644010103284ddbc27038f678d60a1bab19c5416
Alois Duston <alois.duston@parsely.com>
2023-02-17 09:47:50 -0800

    bug fix

--- src/test.py
+++ src/test.py
@@ -39,11 +39,11 @@ print_freq = 10
 
 def run():
     model_params,training_data, validation_data = get_test_params(max_data = 500)
-    train_params = {'bag_epochs': 100, 'pretrain_epochs': 200, 'nbags': 50, 'bag_size':7,
+    train_params = {'bag_epochs': 100, 'pretrain_epochs': 1400, 'nbags': 50, 'bag_size':7,
                     'subtrain_epochs': 40, 'bag_sub_epochs':20, 'bag_learning_rate':.01}
     model_params['batch_size'] = 7
     model_params['threshold_frequency'] = 25
-    model_params['sequential_thresholding'] = False
+    model_params['sequential_thresholding'] = True
     if torch.cuda.is_available():
         l = len(training_data['x'])
         model_params, training_data, validation_data = get_test_params(max_data =5000)
--- src/torch_training.py
+++ src/torch_training.py
@@ -76,7 +76,7 @@ def train_bag_epochs(model, bag_loader, params, train_params):
     Bag_coeffs = []
     for batch_index, bag_data in enumerate(bag_loader):
         bag_model = deepcopy(model)
-        perturbation = .01 * torch.randn(bag_model.sindy_coeffs.shape)
+        perturbation = .01 * torch.randn(bag_model.sindy_coeffs.shape, device = params['device'])
         bag_model.sindy_coeffs = torch.nn.Parameter(perturbation + bag_model.sindy_coeffs, requires_grad = True)
         bag_coeffs = get_bag_coeffs(bag_model, bag_data, params, train_params)
         Bag_coeffs.append(bag_coeffs)
