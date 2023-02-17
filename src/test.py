eca6f48f3a7a329193d7e71742211790a2661bfe
Alois Duston <alois.duston@parsely.com>
2023-02-16 22:27:52 -0800

    bug fix

--- src/test.py
+++ src/test.py
@@ -38,7 +38,7 @@ print_freq = 10
 
 
 def run():
-    model_params,training_data, validation_data = get_test_params(max_data = 5000)
+    model_params,training_data, validation_data = get_test_params(max_data = 500)
     train_params = {'bag_epochs': 100, 'pretrain_epochs': 50, 'nbags': 50, 'bag_size':7,
                     'subtrain_epochs': 40, 'bag_sub_epochs':20, 'bag_learning_rate':.01}
     model_params['batch_size'] = 7
@@ -46,7 +46,7 @@ def run():
     model_params['sequential_thresholding'] = False
     l = len(training_data['x'])
     if torch.cuda.is_available():
-        model_params, training_data, validation_data = get_test_params()
+        model_params, training_data, validation_data = get_test_params(5000)
         model_params['sequential_thresholding'] = False
         train_params = {'bag_epochs': 100, 'pretrain_epochs': 200, 'nbags': l//100, 'bag_size': 100,
                         'subtrain_epochs': 60, 'bag_sub_epochs':30, 'bag_learning_rate':.01}
