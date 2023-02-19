e9a133ea44a313c59ec1993e6e21c3ba2a230080
Alois Duston <alois.duston@parsely.com>
2023-02-19 10:29:56 -0800

    bug fix

--- src/test.py
+++ src/test.py
@@ -39,7 +39,7 @@ warnings.filterwarnings("ignore")
 def BA_small_test(model_params, training_data, validation_data):
     model_params['sequential_thresholding'] = False
     l = len(training_data['x'])
-    train_params = {'bag_epochs': 45, 'pretrain_epochs': 100, 'nbags': (1.5 * l) // 7, 'bag_size': 7,
+    train_params = {'bag_epochs': 120, 'pretrain_epochs': 100, 'nbags': (1.5 * l) // 7, 'bag_size': 7,
                     'subtrain_epochs': 20, 'bag_sub_epochs': 5, 'bag_learning_rate': .01, 'shuffle_threshold': 3}
     model_params['batch_size'] = 7
     model_params['threshold_frequency'] = 25
@@ -72,7 +72,7 @@ def A_test(model_params, training_data, validation_data):
 def A_small_test(model_params, training_data, validation_data):
     model_params['sequential_thresholding'] = True
     l = len(training_data['x'])
-    train_params = {'bag_epochs': 0, 'pretrain_epochs': 1000, 'nbags': int(1.5 * l // 300), 'bag_size': 300,
+    train_params = {'bag_epochs': 0, 'pretrain_epochs': 2500, 'nbags': int(1.5 * l // 300), 'bag_size': 300,
                     'subtrain_epochs': 80, 'bag_sub_epochs': 4, 'bag_learning_rate': .01, 'shuffle_threshold': 5}
     model_params['batch_size'] = 7
     model_params['threshold_frequency'] = 25
