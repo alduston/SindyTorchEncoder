37c41eca5bdfd54cce7572b87b366a75bd2988bb
Alois Duston <alois.duston@parsely.com>
2023-02-21 17:55:01 -0800

    bug fix

--- src/test.py
+++ src/test.py
@@ -175,7 +175,7 @@ def Meta_test(runs = 5, small = False):
 def run():
     if torch.cuda.is_available():
         model_params, training_data, validation_data = get_test_params(max_data=5000)
-        PA_test()
+        PA_test(model_params, training_data, validation_data)
 
     #PAnet, PALoss_dict = PA_small_test(model_params, training_data, validation_data)
 
