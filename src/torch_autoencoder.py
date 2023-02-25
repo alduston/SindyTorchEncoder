165c5a77b72ec71abb383ed889f3ab006a6a6d06
Alois Duston <alois.duston@parsely.com>
2023-02-25 11:08:44 -0800

    bug fix

--- src/torch_autoencoder.py
+++ src/torch_autoencoder.py
@@ -249,7 +249,7 @@ class SindyNet(nn.Module):
             if penalize_self:
                 sub_coeffs = self.sub_model_coeffs[idx]
             else:
-                sub_coeffs = torch.sum(self.sub_model_coeffs, dim = 1)
+                sub_coeffs = torch.sum(self.sub_model_coeffs, dim = 0)
         return self.params['loss_weight_sindy_regularization'] * torch.mean(torch.abs(sub_coeffs))
 
 
