5064b9c02f2b1c47f4ac4bf7825cba5007eabc24
Alois Duston <alois.duston@parsely.com>
2023-02-27 15:00:33 -0800

    generic commit

--- src/test.py
+++ src/test.py
@@ -249,7 +249,7 @@ def get_plots(Meta_A_df, Meta_PA_df, n_runs, exp_label, plot_keys = ["sindy_x_",
 
 def run():
     exp_label='coeff_loss'
-    n_runs = 5
+    n_runs = 1
     #param_updates = {'loss_weight_sindy_z': 0}
     #PAparam_updates = {'coefficient_initialization': 'normal',
                        #'loss_weight_sindy_regularization': 5e-4}
