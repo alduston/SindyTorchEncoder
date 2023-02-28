8e4d3a03e11e0fcab5ba540bdbd12c75a002aa5d
Alois Duston <alois.duston@parsely.com>
2023-02-28 09:32:52 -0800

    generic commit

--- src/test.py
+++ src/test.py
@@ -254,7 +254,7 @@ def run():
     param_updates = {'loss_weight_decoder': 1}
     if torch.cuda.is_available():
         Meta_A_df, Meta_PA_df = Meta_test(runs=n_runs, exp_label=exp_label, param_updates= param_updates,
-                                          exp_size=(256, np.inf), PAparam_updates = PAparam_updates)
+                                          exp_size=(128, np.inf), PAparam_updates = PAparam_updates)
     else:
         try:
             os.mkdir(f'../plots/{exp_label}')
