5717f2f9bb9cb016d94e87fb32700cd026dbceaf
Alois Duston <alois.duston@parsely.com>
2023-02-28 11:56:22 -0800

    generic commit

--- src/test.py
+++ src/test.py
@@ -251,7 +251,7 @@ def run():
     exp_label='alt_reg'
     n_runs = 5
     PAparam_updates = {'coefficient_initialization': 'constant'}
-    param_updates = {'loss_weight_decoder': 1}
+    param_updates = {'loss_weight_decoder': .1}
     if torch.cuda.is_available():
         Meta_A_df, Meta_PA_df = Meta_test(runs=n_runs, exp_label=exp_label, param_updates= param_updates,
                                           exp_size=(64, np.inf), PAparam_updates = PAparam_updates)
