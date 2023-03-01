d6e7d69a8959f112a2d57b8befa35b390571810f
Alois Duston <alois.duston@parsely.com>
2023-03-01 11:05:36 -0800

    generic commit

--- src/test.py
+++ src/test.py
@@ -248,10 +248,10 @@ def get_plots(Meta_A_df, Meta_PA_df, n_runs, exp_label, plot_keys = ["sindy_x_",
 
 
 def run():
-    exp_label='normal_init'
+    exp_label='sanity_check'
     n_runs = 6
-    param_updates = {'loss_weight_decoder': .1}
-    PAparam_updates = {'coefficient_initialization': 'normal'}
+    param_updates = {'loss_weight_decoder': 1}
+    PAparam_updates = {'coefficient_initialization': 'constant'}
     if torch.cuda.is_available():
         Meta_A_df, Meta_PA_df = Meta_test(runs=n_runs, exp_label=exp_label, exp_size=(200, np.inf),
                                           param_updates = param_updates, PAparam_updates = PAparam_updates)
