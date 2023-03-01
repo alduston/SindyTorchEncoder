528c76cec3dd487aaf3a7890c217f6337aa88d20
Alois Duston <alois.duston@parsely.com>
2023-02-28 20:03:06 -0800

    generic commit

--- src/torch_autoencoder.py
+++ src/torch_autoencoder.py
@@ -255,7 +255,7 @@ class SindyNet(nn.Module):
             else:
                 #sub_coeffs = torch.sum(self.sub_model_coeffs, dim = 0) * (1/self.params['nbags'])
                 sub_coeffs = self.sub_model_coeffs[idx]
-                sub_coeffs = sub_coeffs + torch.sum(deepcopy(self.sub_model_coeffs), dim=0) - deepcopy(sub_coeffs)
+                sub_coeffs = sub_coeffs + torch.sum(copy(self.sub_model_coeffs), dim=0) - copy(sub_coeffs)
         return self.params['loss_weight_sindy_regularization'] * torch.mean(torch.abs(sub_coeffs))
 
 
