cb1446a66a010dbfb82b5719caca810db1ed7b9d
Alois Duston <alois.duston@parsely.com>
2023-03-04 11:46:15 -0800

    generic commit

--- src/test.py
+++ src/test.py
@@ -248,9 +248,9 @@ def get_plots(Meta_A_df, Meta_PA_df, n_runs, exp_label, plot_keys = ["sindy_x_",
 
 def run():
     #NOTE eurak comit was f76ebcf
-    exp_label= 'eureka_pt2'
+    exp_label= 'eureka_alt'
     n_runs = 10
-    param_updates = {'loss_weight_decoder': 1}
+    param_updates = {'loss_weight_decoder': .1}
     PAparam_updates = {'coefficient_initialization': 'constant'}
     if torch.cuda.is_available():
         Meta_A_df, Meta_PA_df = Meta_test(runs=n_runs, exp_label=exp_label, exp_size=(128, np.inf),
