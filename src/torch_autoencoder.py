dffe1515fed5fcbb527b0f75e622e45191ac37ac
Alois Duston <alois.duston@parsely.com>
2023-03-21 13:03:50 -0700

    generic commit

--- src/torch_autoencoder.py
+++ src/torch_autoencoder.py
@@ -206,7 +206,7 @@ class SindyNet(nn.Module):
                 self.num_active_coeffs = torch.sum(copy(self.coefficient_mask)).cpu().detach().numpy()
         if scramble:
             predict = torch.einsum('ij,jk->ik', Theta, self.coefficient_mask * sindy_coefficients)
-
+            print(predict.shape)
             print(torch.matmul(Theta, self.coefficient_mask * sindy_coefficients).shape)
         return torch.matmul(Theta, self.coefficient_mask * sindy_coefficients)
 
