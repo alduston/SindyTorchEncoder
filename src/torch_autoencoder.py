948b17a26e77a9f801de56be856cc0414cf8b0ab
Alois Duston <alois.duston@parsely.com>
2023-03-21 09:30:56 -0700

    generic commit

--- src/torch_autoencoder.py
+++ src/torch_autoencoder.py
@@ -211,7 +211,7 @@ class SindyNet(nn.Module):
                 self.coefficient_mask = self.coefficient_mask * torch.tensor(torch.abs(sindy_coefficients) >= self.params['coefficient_threshold'], device=self.device)
                 self.num_active_coeffs = torch.sum(copy(self.coefficient_mask)).cpu().detach().numpy()
 
-        self.dist_mult((copy(Theta), copy(self.coefficient_mask * self.sub_model_coeffs)))
+        self.dist_mult(copy(Theta), copy(self.coefficient_mask * self.sub_model_coeffs))
         return torch.matmul(Theta, self.coefficient_mask * sindy_coefficients)
 
 
