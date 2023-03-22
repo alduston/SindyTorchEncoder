49fa00be8dc55c754ff5594e532921d3226983d8
Alois Duston <alois.duston@parsely.com>
2023-03-22 14:46:14 -0700

    generic commit

--- src/scramble_test.py
+++ src/scramble_test.py
@@ -171,7 +171,7 @@ def get_plots(Meta_A_df, Meta_PA_df, n_runs, exp_label, plot_keys = ["sindy_x_",
 def run():
     PAparam_updates = {'coefficient_initialization': 'xavier'}
     param_updates = {'loss_weight_decoder': .1}
-    n_runs = 5
+    n_runs = 1
     exp_label = 'masked_ensemble_one_run'
 
 
