bf40fbf85f9ed5f8943d0f215c42d3b58ef839ff
Alois Duston <alois.duston@parsely.com>
2023-07-20 18:26:24 -0700

    generic commit

--- src/ensemble_model.py
+++ src/ensemble_model.py
@@ -646,10 +646,11 @@ class SindyNetEnsemble(nn.Module):
         x_decode, z, z_stack = self.forward(x)
         x_stack, dx_stack, z_stack_alt, decode_stack = self.alt_forward(x, dx)
 
-        #decoder_loss = self.alt_decoder_loss(x_stack, decode_stack)
-        #sindy_x_loss = self.alt_sindy_x_loss(x_stack, dx_stack)
-        decoder_loss = self.decoder_loss(x, x_decode)
-        sindy_x_loss = self.sindy_x_loss(z, x, dx, ddx)
+        decoder_loss = self.alt_decoder_loss(x_stack, decode_stack)
+        sindy_x_loss = self.alt_sindy_x_loss(x_stack, dx_stack)
+
+        #decoder_loss = self.decoder_loss(x, x_decode)
+        #sindy_x_loss = self.sindy_x_loss(z, x, dx, ddx)
 
         sindy_z_loss = self.sindy_z_loss(z, x, dx, ddx)
         reg_loss = self.sindy_reg_loss(alt = False)
