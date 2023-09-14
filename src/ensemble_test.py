a8ed27ecd35c46c034edcacf8379a49e63bcc0f8
Alois Duston <alois.duston@parsely.com>
2023-09-13 17:23:37 -0700

    generic commit

--- src/ensemble_test.py
+++ src/ensemble_test.py
@@ -354,9 +354,9 @@ def basic_test(exp_label = 'exp', model_save_name = 'model0', small = False):
                          'n_encoders': 25, 'n_decoders': 25, 'criterion': 'avg', 's1_epochs': 201,
                          'test_freq': 100, 'exp_label': 'exp', 's2_epochs': 0, 'crossval_freq': 100}
     else:
-        params, training_data, validation_data = get_lorenz_params(train_size=40, test_size=20)
+        params, training_data, validation_data = get_lorenz_params(train_size=30, test_size=15)
         params_update = {'replacement': True, 'coefficient_initialization': 'constant', 'pretrain_epochs': 200,
-                         'n_encoders': 15, 'n_decoders': 15, 'criterion': 'avg', 's1_epochs': 10001,
+                         'n_encoders': 7, 'n_decoders': 7, 'criterion': 'avg', 's1_epochs': 5000,
                          'test_freq': 100, 'exp_label': 'exp', 's2_epochs': 0, 'crossval_freq': 100}
 
     params.update(params_update)
@@ -383,7 +383,7 @@ def get_step1_min_losses(item_loss_dict):
 
 def run():
     #basic_test(exp_label='model4', model_save_name='model_exp_med', small = False)
-    indep_model, bag_loader, test_loader = load_model('small_model')
+    indep_model, bag_loader, test_loader = load_model('model4')
     net, Loss_dict,  E_loss_dict0 = train_eas_1(indep_model, bag_loader, test_loader, model_params = {'s1_epochs': 1})
     item_loss_dict = net.item_loss_dict
     min_losses = get_step1_min_losses(item_loss_dict)
@@ -392,7 +392,7 @@ def run():
                   'active_coeffs': Loss_dict['active_coeffs'][-1]}
     print(s_1_losses)
 
-    indep_model, bag_loader, test_loader = load_model('small_model')
+    indep_model, bag_loader, test_loader = load_model('model4')
 
     indep_model.params['coefficient_initialization'] = 'constant'
     indep_model.params['criterion'] = 'avg'
@@ -408,7 +408,7 @@ def run():
                                                                        test_loader, compressor_model.params)
         E_loss_dicts.append(E_loss_dict1)
 
-    step_2_plots(E_loss_dicts,E_loss_dict0, s_1_losses, exp_label='plot_exp_small')
+    step_2_plots(E_loss_dicts,E_loss_dict0, s_1_losses, exp_label='plot_exp_med')
 
 
 if __name__=='__main__':
